3c0fb27905d402f6e25ca6d2e9a6a7891d8df57f
Martín Basualdo <70729951+MartinBasualdo0@users.noreply.github.com>
2024-02-01 23:57:44 -0300

    actualization and test for main_scrap_zonaprop

--- src/constants.py
+++ src/constants.py
@@ -1,2 +1,2 @@
-zona_prop_url = "https://www.zonaprop.com.ar/inmuebles-alquiler-capital-federal.html"
+zona_prop_url = "https://www.zonaprop.com.ar/departamentos-alquiler-capital-federal.html"
 max_number_pages_zonaprop = 1_000_000
