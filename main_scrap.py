f00a5ea712e07f43dfc44ceef6e32fb1d77b8297
MartinBasualdo0 <martin.basualdo@hotmail.com>
2024-02-04 21:03:35 -0300

    main for selling apartments

--- main_scrap.py
+++ main_scrap.py
@@ -1,5 +1,8 @@
 from src.zonaprop import main_scrap_zonaprop
 
 if __name__ == "__main__":
-    main_scrap_zonaprop(type_building="departamentos",
-                    export_final_results=True)
+    main_scrap_zonaprop(
+        type_operation="venta",
+        type_building="departamentos",
+        export_final_results=True
+        )
