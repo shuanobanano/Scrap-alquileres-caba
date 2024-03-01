ad3d51006ba351c0152f1cf4f0740264da1b303b
MartinBasualdo0 <martin.basualdo@hotmail.com>
2024-02-29 22:03:52 -0300

    scrap for rent

--- main_scrap.py
+++ main_scrap.py
@@ -2,7 +2,7 @@ from src.zonaprop import main_scrap_zonaprop
 
 if __name__ == "__main__":
     main_scrap_zonaprop(
-        type_operation="venta",
+        type_operation="alquiler",
         type_building="departamentos",
         export_final_results=True
         )
