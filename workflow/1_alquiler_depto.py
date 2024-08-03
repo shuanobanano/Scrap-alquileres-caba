ca31719a0c1c4fc4cc1c41fa7df06afbc28cb32f
MartinBasualdo0 <martin.basualdo@hotmail.com>
2024-08-03 10:19:35 -0300

    segmented workflow

---
+++ workflow/1_alquiler_depto.py
@@ -0,0 +1,14 @@
+import sys
+import os
+
+# Añade la ruta del directorio 'project_root' al sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from src.zonaprop import main_scrap_zonaprop
+
+if __name__ == "__main__":
+    main_scrap_zonaprop(
+        type_operation="alquiler",
+        type_building="departamentos",
+        export_final_results=True
+        )
