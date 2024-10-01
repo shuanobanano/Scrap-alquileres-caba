275109c4f5e9c032a59edc5df0b8e535c0b5a483
MartinBasualdo0 <martin.basualdo@hotmail.com>
2024-09-30 22:39:00 -0300

    time log

--- main_scrap.py
+++ main_scrap.py
@@ -1,6 +1,8 @@
 from src.zonaprop import main_scrap_zonaprop
+import time
 
 if __name__ == "__main__":
+    inicio = time.time()
     main_scrap_zonaprop(
         type_operation="alquiler",
         type_building="departamentos",
@@ -37,3 +39,13 @@ if __name__ == "__main__":
         type_building="oficinas-comerciales",
         export_final_results=True
         )
+    
+    final = time.time()
+    tiempo_total = final - inicio
+    # Convertir el tiempo a formato horas: minutos: segundos
+    horas, resto = divmod(tiempo_total, 3600)
+    minutos, segundos = divmod(resto, 60)
+    
+    # Guardar el tiempo de ejecución en un archivo .txt
+    with open("tiempo_ejecucion.txt", "w") as file:
+        file.write(f"Tiempo total de ejecución: {int(horas)}:{int(minutos)}:{int(segundos)}\n")
